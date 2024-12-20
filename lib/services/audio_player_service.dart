9ae71f6a4908dbd7b767898b8c0146b03eab1d23
Timur Dyushaliev <timurdyushaliev@gmail.com>
2024-12-20 11:37:06 +0600

    fix: Audio

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -1,15 +1,18 @@
 import 'package:just_audio/just_audio.dart';
 
-class AudioPlayerService {
+final class AudioPlayerService {
+  AudioPlayerService._();
+
+  static final AudioPlayerService _instance = AudioPlayerService._();
+
+  factory AudioPlayerService() => _instance;
+
   final AudioPlayer _audioPlayer = AudioPlayer();
 
   Future<void> playAudio() async {
+    if (_audioPlayer.playing) return;
     await _audioPlayer.setLoopMode(LoopMode.one);
     await _audioPlayer.setAsset('assets/let_it_snow.mp3');
     await _audioPlayer.play();
-  }
-
-  Future<void> stopAudio() async {
-    await _audioPlayer.stop();
   }
 }
